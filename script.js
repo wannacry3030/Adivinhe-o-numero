899b758e7e684ce315e04ce84a52be5986c6291c
wannacry3030 <wannacry.bat@proton.me>
2023-03-06 18:37:00 -0300

    att

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ document.querySelector('.message').textContent = '🎉 Voce Acertou!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
