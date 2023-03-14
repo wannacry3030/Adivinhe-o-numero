35c84bf5052e16dde65fec68acb21b4a1ec872d2
wannacry3030 <wannacry.bat@proton.me>
2023-03-14 12:50:32 -0300

    att

--- script.js
+++ script.js
@@ -10,6 +10,9 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -17,5 +20,7 @@ document.querySelector('.check').addEventListener('click', function () {
   // caso não tenha um numero digitado
   if (!guess) {
     document.querySelector('.message').textContent = ' Digite um numero!!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Voce Acertou!';
   }
 });
