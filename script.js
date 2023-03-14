58beb31b0c27e832bcedfd0ab445fc4788129d35
wannacry3030 <wannacry.bat@proton.me>
2023-03-14 12:38:54 -0300

    att

--- script.js
+++ script.js
@@ -11,6 +11,6 @@
 // console.log(document.querySelector('.guess').value);
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
-  console.log(guess);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 });
