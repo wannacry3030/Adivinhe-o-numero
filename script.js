d1c0d4cff4437ba2ce1fb5c2950c1f00c8fff7da
wannacry3030 <wannacry.bat@proton.me>
2023-03-14 12:28:53 -0300

    att

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Voce Acertou!';
+// document.querySelector('.message').textContent = '🎉 Voce Acertou!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
