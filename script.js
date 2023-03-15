47feaca3bbadac2f183853e02ea4ea7dcadb2e0a
wannacry3030 <wannacry.bat@proton.me>
2023-03-15 16:32:26 -0300

    att

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
   document.querySelector('.guess').value = '';
   document.querySelector('.message').textContent = 'Comece a adivinhar...';
-  document.querySelector('.score').textContent = '20';
+  document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
@@ -54,7 +55,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // quando o jogador chuta um numero muito baixo
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Muito baixo! ☝️';
+      document.querySelector('.message').textContent = 'Muito baixo! 👇';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
